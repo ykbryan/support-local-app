5b1d0c715dbb7a2dd50126a5b231f740d3f56543
Bryan Chua <me@bryanchua.com>
2020-04-15 00:17:49 +0800

    Updating UI to include filter and search

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,11 +7,14 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import { FilterLeft } from 'react-bootstrap-icons';
 
 export default function Home() {
   let { merchantName } = useParams();
   const [loading, setLoading] = useState(false);
   const [selectionIndex, setSelectionIndex] = useState(1);
+  const [showModal, setShowModal] = useState(false);
 
   const isButtonSelected = (index) => {
     if (selectionIndex === index) return true;
@@ -22,11 +25,15 @@ export default function Home() {
     setSelectionIndex(index);
   };
 
+  const handleFilter = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <Container>
-      <Row className='p-3'>
+      <Row className=''>
         <Col>
-          <h2>#SupportLocalSG</h2>
+          <h2 className='p-3'>#SupportLocalSG</h2>
           <p>
             Our hawker uncles and aunties are cooking up a storm in the midst of
             Covid-19! Come order your makan from them and support our local F&B
@@ -42,7 +49,7 @@ export default function Home() {
         </Col>
       </Row>
       <Row id='selections'>
-        <Col>
+        <Col xs={12} md={7}>
           <Button
             active={isButtonSelected(0)}
             onClick={() => buttonPressed(0)}
@@ -89,42 +96,185 @@ export default function Home() {
             <span className='p-2'>Fresh Produce & Groceries</span>
           </Button>
         </Col>
+        <Col xs={12} md={5} className='text-right'>
+          <Button
+            className='m-1'
+            disabled={loading}
+            variant='outline-secondary'
+            onClick={handleFilter}
+          >
+            <FilterLeft className='ml-12 m-1' />
+            Filter
+          </Button>
+          <Modal className='filterModal' show={showModal} onHide={handleFilter}>
+            <Modal.Header closeButton>
+              <Modal.Title>Refine your search</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form id='filterForm'>
+                <Row className='filterName'>
+                  <Col>
+                    <strong>CUISINE</strong>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Check
+                      inline
+                      label='All'
+                      type='checkbox'
+                      id={`cuisine-checkbox-1`}
+                    />
+                    <Form.Check
+                      inline
+                      label='Asian'
+                      type='checkbox'
+                      id={`cuisine-checkbox-1`}
+                    />
+                    <Form.Check
+                      inline
+                      label='Seafood'
+                      type='checkbox'
+                      id={`cuisine-checkbox-2`}
+                    />
+                    <Form.Check
+                      inline
+                      label='Vegetarian'
+                      type='checkbox'
+                      id={`cuisine-checkbox-3`}
+                    />
+                    <Form.Check
+                      inline
+                      label='Something'
+                      type='checkbox'
+                      id={`cuisine-checkbox-4`}
+                    />
+                  </Col>
+                </Row>
+                <Row className='filterName'>
+                  <Col>
+                    <strong>LOCATION</strong>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Check
+                      inline
+                      label='All'
+                      type='checkbox'
+                      id={`cuisine-checkbox-1`}
+                    />
+                    <Form.Check
+                      inline
+                      label='Central'
+                      type='checkbox'
+                      id={`cuisine-checkbox-1`}
+                    />
+                    <Form.Check
+                      inline
+                      label='North'
+                      type='checkbox'
+                      id={`cuisine-checkbox-1`}
+                    />
+                    <Form.Check
+                      inline
+                      label='South'
+                      type='checkbox'
+                      id={`cuisine-checkbox-1`}
+                    />
+                    <Form.Check
+                      inline
+                      label='East'
+                      type='checkbox'
+                      id={`cuisine-checkbox-1`}
+                    />
+                    <Form.Check
+                      inline
+                      label='West'
+                      type='checkbox'
+                      id={`cuisine-checkbox-1`}
+                    />
+                  </Col>
+                </Row>
+                <Row className='filterName'>
+                  <Col xs={12} md={6}>
+                    <strong>MIN ORDER</strong>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={12} md={6}>
+                    <Button
+                      active={isButtonSelected(0)}
+                      onClick={() => buttonPressed(0)}
+                      className='m-1'
+                      disabled={loading}
+                      variant='outline-secondary'
+                    >
+                      <span className='p-2'>$0</span>
+                    </Button>
+                    <Button
+                      active={isButtonSelected(0)}
+                      onClick={() => buttonPressed(0)}
+                      className='m-1'
+                      disabled={loading}
+                      variant='outline-secondary'
+                    >
+                      <span className='p-2'>$20</span>
+                    </Button>
+                    <Button
+                      active={isButtonSelected(0)}
+                      onClick={() => buttonPressed(0)}
+                      className='m-1'
+                      disabled={loading}
+                      variant='outline-secondary'
+                    >
+                      <span className='p-2'>$50</span>
+                    </Button>
+                  </Col>
+                </Row>
+                <Row className='filterName'>
+                  <Col>
+                    <strong>SORT BY</strong>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Button
+                      active={isButtonSelected(0)}
+                      onClick={() => buttonPressed(0)}
+                      className='m-1'
+                      disabled={loading}
+                      variant='outline-secondary'
+                    >
+                      <span className='p-2'>Name</span>
+                    </Button>
+                    <Button
+                      active={isButtonSelected(0)}
+                      onClick={() => buttonPressed(0)}
+                      className='m-1'
+                      disabled={loading}
+                      variant='outline-secondary'
+                    >
+                      <span className='p-2'>Min. Order</span>
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant='secondary' onClick={handleFilter}>
+                Cancel
+              </Button>
+              <Button variant='primary' onClick={handleFilter}>
+                Apply
+              </Button>
+            </Modal.Footer>
+          </Modal>
+          <Form id='searchbar' className='m-1'>
+            <Form.Control placeholder='Search...' />
+          </Form>
+        </Col>
       </Row>
-      <Form id='filterForm'>
-        <Row>
-          <Col xs={12} md={3}>
-            <Form.Group
-              className='filterCuisine'
-              as={Col}
-              controlId='formSelectCuisine'
-            >
-              <Form.Control as='select' value='Choose Cuisine'>
-                <option>Choose Cuisine</option>
-                <option>...</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-          <Col xs={12} md={3}>
-            <Form.Group
-              className='filterLocation'
-              as={Col}
-              controlId='formSelectLocation'
-            >
-              <Form.Control as='select' value='Choose Location'>
-                <option>Choose Location</option>
-                <option>...</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-          <Col xs={12} md={6}>
-            <Form.Group className='filterPrice' controlId='formPriceRange'>
-              <Form.Label>Min Order</Form.Label>
-              <Form.Control type='range' value='100' />
-              <Form.Label>$100</Form.Label>
-            </Form.Group>
-          </Col>
-        </Row>
-      </Form>
       <Row className='list-merchants'>
         <Col xs={12} md={6}>
           <Card className='merchant-card'>
